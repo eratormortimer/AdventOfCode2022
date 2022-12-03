a5e6ed838d7c54c6b3c8b320891a6e29e2941a2f
Erator <simon.sternsdorf@googlemail.com>
2022-12-03 21:48:18 +0100

    better readablity

--- 3.rs
+++ 3.rs
@@ -1,11 +1,12 @@
 
 use std::collections::HashSet;
+use std::ops::BitAnd;
 
 fn share_char(a: &str, b: &str, c: &str) -> char {
     let set1: HashSet<char> = a.chars().collect();
     let set2: HashSet<char> = b.chars().collect();
     let set3: HashSet<char> = c.chars().collect();
-    *( &(&set1 & &set2) & &set3).iter().next().unwrap()
+    *set1.bitand(&set2).bitand(&set3).iter().next().unwrap()
     //set1.intersection(&set2).collect::<HashSet<char>>().iter().next().unwrap()
 
 
@@ -15,21 +16,15 @@ fn main() {
     let content = std::fs::read_to_string("input_3.txt")
         .expect("file not found!");
     let mut points = 0;
-    let mut count = 0;
-    let mut strings = Vec::new();
-    for line in content.lines() {
-        strings.push(line);
-        count += 1;
-        if count == 3 {
-            let char_result= share_char(strings[0], strings[1], strings[2]) as u32;
-            match char_result {
-                97..=122 => points += char_result - (70 + 26),
-                _ => points += char_result - 64 + 26,
-            }
-            strings.clear();
-            count = 0;
+    let lines : Vec<_> = content.lines().collect();
+    
+    for line in lines.chunks(3) {
+        let char_result= share_char(line[0], line[1], line[2]) as u32;
+        match char_result {
+            97..=122 => points += char_result - (70 + 26),
+            _ => points += char_result - 64 + 26,
         }
-       
     }
+
     println!("{}", points);
 }
