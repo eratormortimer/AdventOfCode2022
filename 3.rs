27152bc1feb5fde7873b8d40d210a4a9c77323f4
Erator <simon.sternsdorf@googlemail.com>
2022-12-03 21:01:05 +0100

    Solved 3.2

--- 3.rs
+++ 3.rs
@@ -1,23 +1,32 @@
 
 use std::collections::HashSet;
 
-fn share_char(a: &str, b: &str) -> char {
+fn share_char(a: &str, b: &str, c: &str) -> char {
     let set1: HashSet<char> = a.chars().collect();
     let set2: HashSet<char> = b.chars().collect();
-    *(&set1 & &set2).iter().next().unwrap()
+    let set3: HashSet<char> = c.chars().collect();
+    *( &(&set1 & &set2) & &set3).iter().next().unwrap()
 }
 
 fn main() {
     let content = std::fs::read_to_string("input_3.txt")
         .expect("file not found!");
     let mut points = 0;
+    let mut count = 0;
+    let mut strings = Vec::new();
     for line in content.lines() {
-        let (split1, split2) = line.split_at(line.len()/2);
-        let char_result= share_char(split1, split2) as u32;
-        match char_result {
-            97..=122 => points += char_result - (70 + 26),
-            _ => points += char_result - 64 + 26,
+        strings.push(line);
+        count += 1;
+        if count == 3 {
+            let char_result= share_char(strings[0], strings[1], strings[2]) as u32;
+            match char_result {
+                97..=122 => points += char_result - (70 + 26),
+                _ => points += char_result - 64 + 26,
+            }
+            strings.clear();
+            count = 0;
         }
+       
     }
     println!("{}", points);
 }
